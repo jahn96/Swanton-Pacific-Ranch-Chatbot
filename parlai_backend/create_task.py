b79c9e3d74000603b1af8d74059fa0c0ccaf170d
mathurma <mathurma@calpoly.edu>
2021-11-10 13:13:34 -0800

    added teacher

--- parlai_backend/create_task.py
+++ parlai_backend/create_task.py
@@ -20,16 +20,78 @@ with open(worksheet_name + '.tsv', 'r') as read_obj:
 
 # Convert data to ParlAI Dialogue format
 with open(worksheet_name + ".txt", 'w') as f:
-  for j in range(len(rows)):
-    for i in range(0,len(rows[j]),2):
-      if rows[j][i] == "":
-        break
-      if j > 0:
-        f.write("\n")
-      f.write("text:")
-      f.write(rows[j][i])
-      f.write("\t")
-      f.write("labels:")
-      if i < len(rows[j]):
-        f.write(rows[j][i+1])
-    f.write("\tepisode_done:True")
+    for j in range(len(rows)):
+        for i in range(0, len(rows[j]), 2):
+            if rows[j][i] == "":
+                break
+            if j > 0:
+                f.write("\n")
+            f.write("text:")
+            f.write(rows[j][i])
+            f.write("\t")
+            f.write("labels:")
+            if i < len(rows[j]):
+                f.write(rows[j][i + 1])
+        f.write("\tepisode_done:True")
+
+def _path(opt, filtered):
+    # set up path to data (specific to each dataset)
+    return worksheet_name + ".txt"
+
+@register_teacher("spr_teacher")
+class SPRTeacher(ParlAIDialogTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+
+        # get datafile
+        opt['parlaidialogteacher_datafile'] = _path(opt, '')
+
+        super().__init__(opt, shared)
+
+
+DisplayData.main(task="spr_teacher")
+#
+#
+# TrainModel.main(
+#   # similar to before
+#   task='spr_teacher',
+#   model='transformer/generator',
+#   model_file='transfer_base/model',
+#
+#   # initialize with a pretrained model
+#   init_model='zoo:blender/blender_90M/model',
+#
+#   # arguments we get from the pretrained model.
+#   # Unfortunately, these must be looked up separately for each model.
+#   n_heads=16, n_layers=8, n_positions=512, text_truncate=512,
+#   label_truncate=128, ffn_size=2048, embedding_size=512,
+#   activation='gelu', variant='xlm',
+#   dict_lower=True, dict_tokenizer='bpe',
+#   dict_file='zoo:blender/blender_90M/model.dict',
+#   learn_positional_embeddings=True,
+#
+#   # some training arguments, specific to this fine-tuning
+#   # use a small learning rate with ADAM optimizer
+#   lr=1e-5, optimizer='adam',
+#   warmup_updates=100,
+#   # early stopping on perplexity
+#   validation_metric='ppl',
+#   # train at most 10 minutes, and validate every 0.25 epochs
+#   max_train_time=600, validation_every_n_epochs=0.25,
+#
+#   # depend on your gpu. If you have a V100, this is good
+#   batchsize=12, fp16=True, fp16_impl='mem_efficient',
+#
+#   # speeds up validation
+#   skip_generation=True,
+#
+#   # helps us cram more examples into our gpu at a time
+#   dynamic_batching='full',
+# )
+
+# DisplayModel.main(
+#     task='spr_teacher',
+#     model='parlai_backend/model',
+#     num_examples=2,
+#     # skip_generation=False,
+# )
