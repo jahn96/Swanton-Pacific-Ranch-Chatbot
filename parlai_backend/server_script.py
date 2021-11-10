91447cd9137449cc6d12fc202dec75682751040c
mathurma <mathurma@calpoly.edu>
2021-11-10 13:22:12 -0800

    launch a service

---
+++ parlai_backend/server_script.py
@@ -0,0 +1,3 @@
+import os
+# os.system("python servies/terminal_chat/run.py --config-path service_components/chatbot/config.yml")
+os.system("python services/browser_chat/run.py --config-path service_components/chatbot/config.yml")
