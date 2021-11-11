61543746ce438769b1fc60133c5a8bbfce2d3071
mathurma <43552384+mathurma@users.noreply.github.com>
2021-11-11 11:01:32 -0800

    Parlai backend (#1)
    * parlai data parser and gitignore
    
    * updated dependecies, created todo
    
    * ignore logs
    
    * added teacher
    
    * broken - teacher not registered
    
    * restructured directory
    
    * ParlAI boilerplate
    
    * make dir Python package
    
    * launch a service
    
    * first model
    
    
    Former-commit-id: b9a2e9a50afe2cea7f4f1de8a48a29e2a8c51e1e
    
    * restructured dir
    
    
    Former-commit-id: 86ac49b5e6be60ead4f0a833c543008f2f2daf9a
    
    * too large
    
    
    Former-commit-id: 8692760cfa43d6db47bc3bbd8247372f85d4572b
    
    * used 'BFG Repo-Cleaner' by Roberto Tyley to wipe the too-large model file from git history. Worked like a charm.
    
    (No conflicts. Can be auto-merged.)

---
+++ parlai_backend/server_script.py
@@ -0,0 +1,3 @@
+import os
+os.system("python services/terminal_chat/run.py --config-path service_components/chatbot/config.yml")
+# os.system("python services/browser_chat/run.py --config-path service_components/chatbot/config.yml")
